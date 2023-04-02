146093070dde71546d2ad4f0b71aa8b575377418
Thomas <thliang01@gmail.com>
2023-04-02 23:34:34 +0800

    feat(app): add app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+# app.py
+import gradio as gr
+import torch
+import requests
+from PIL import Image
+from torchvision import transforms
+
+model = torch.hub.load('pytorch/vision', 'resnet18', pretrained=True).eval()
+
+# Download human-readable labels for ImageNet.
+response = requests.get("https://git.io/JJkYN")
+labels = response.text.split("\n")
+
+def predict(inp):
+  inp = transforms.ToTensor()(inp).unsqueeze(0)
+  with torch.no_grad():
+    prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
+    confidences = {labels[i]: float(prediction[i]) for i in range(1000)}    
+  return confidences
+
+# create gradio interface, with text input and dict output
+gr.Interface(fn=predict, 
+             inputs=gr.Image(type="pil"),
+             outputs=gr.Label(num_top_classes=3),
+             examples=["lion.jpg", "cheetah.jpg"]).launch()
+
+# run the app
+gr.launch(server_port=8080, enable_queue=False)
